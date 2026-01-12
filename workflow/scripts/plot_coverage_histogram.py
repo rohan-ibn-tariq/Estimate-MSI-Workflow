363faaef5b1887538e4e52684783445ea75912ae
rohan-ibn-tariq <178284954+rohan-ibn-tariq@users.noreply.github.com>
2026-01-12 21:15:16 +0500

    change display of coverage plot

--- workflow/scripts/plot_coverage_histogram.py
+++ workflow/scripts/plot_coverage_histogram.py
@@ -31,13 +31,20 @@ df = pl.read_csv(
     }
 )
 
-histogram_data = df.select(["fraction"])
+histogram_data = df.select(["overlap_count"])
+
+max_reads = df["overlap_count"].max()
 
 chart = alt.Chart(histogram_data).mark_bar().encode(
-    x=alt.X('fraction:Q', bin=True, title='Coverage Fraction'),
+    x=alt.X(
+        'overlap_count:Q',
+        bin=alt.Bin(maxbins=100),
+        title='Number of Reads Overlapping MS Region',
+        scale=alt.Scale(domain=[0, max_reads])
+    ),
     y=alt.Y('count()', title='Number of MS Regions')
 ).properties(
-    width=700,
+    width=800,
     height=400,
     title=title
 )
